a07d5a79b39d65357b00a75b21f8e6d769e7e8e8
arifrahim <arifrahimjamal@gmail.com>
2025-03-15 12:49:24 +0300

    tried to do the code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const toDos = [];
+  // created an array to store the inputed data as a list.
+
+  const input = document.querySelector("#new-task-description"); // selected the input element with the id "#new-task-description"
+
+  const submitButton = document.querySelector("#submit-button"); // selected the input element with the id "#submit-button"
+
+  const taskList = document.querySelector("#tasks");
+
+  submitButton.addEventListener("click", function () {
+    // adds the input to the array(toDos) when the submit button is clicked.
+    toDos.push(input.value);
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    taskList.appendChild(li);
+  });
 });
